4e2523cf9ae99f52f77e4f696fc534944665b65d
Wahyu Dwi Nugraha <nugrahawd9@gmail.com>
2026-05-27 22:26:20 +0700

    Fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.wahyu.docsguard',
+  appId: 'com.docsguard',
   appName: 'docs-guard',
   webDir: 'out',
   plugins: {
