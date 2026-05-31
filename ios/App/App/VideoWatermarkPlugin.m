df4c9abbe40843580b28bf6aa8cc120f35fcfa00
test <test@gmail.com>
2026-05-31 08:56:07 +0000

    fix: restore semicolons in Objective-C plugin registration to fix compilation error

--- ios/App/App/VideoWatermarkPlugin.m
+++ ios/App/App/VideoWatermarkPlugin.m
@@ -2,6 +2,6 @@
 #import <Capacitor/Capacitor.h>
 
 CAP_PLUGIN(VideoWatermark, "VideoWatermark",
-           CAP_PLUGIN_METHOD(addTextWatermark, CAPPluginReturnPromise)
-           CAP_PLUGIN_METHOD(ping, CAPPluginReturnPromise)
+           CAP_PLUGIN_METHOD(addTextWatermark, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(ping, CAPPluginReturnPromise);
 )
